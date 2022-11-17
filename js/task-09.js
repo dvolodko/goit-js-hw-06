e68ee7188849362c58e4ed60309733622dee8a6f
Denys Volodko <babyleejane@gmail.com>
2022-11-17 21:59:54 +0200

    tasks 8-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const refs = {
+	button: document.querySelector(".change-color"),
+	colorDisplay: document.querySelector(".color"),
+};
+
+refs.button.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+	refs.colorDisplay.textContent = getRandomHexColor();
+	document.body.style.backgroundColor = getRandomHexColor();
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
